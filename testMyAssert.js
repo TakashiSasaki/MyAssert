1ec5836416e119140fa7be88f9af011e4b22ff76
Takashi SASAKI <takashi316@gmail.com>
2018-04-11 22:50:05 +0900

    testMyAssert passed on nodejs

--- testMyAssert.js
+++ testMyAssert.js
@@ -1,10 +1,15 @@
+if(typeof assert === "undefined") require("myassert");
+
 function testMyAssert(){
   console.log("testMyAssert");
-  if(typeof assert === "undefined") require("myassert");
   
   assert.isString("abc");
   assert.isObject({});
   assert.isStringArray(["ab", "cd"]);
+}
+
+if(typeof process !== "undefined") {
+  testMyAssert();
 }
 
 
